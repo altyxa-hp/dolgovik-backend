8b8b2506523582228c9c20e3a96c3769abccc952
Altyn <altynbeksadykov05@gmail.com>
2026-05-19 16:14:26 +0600

    migrations in build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ set -o errexit  # Остановить при ошибке
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+python manage.py makemigrations api
+
 python manage.py migrate
+
